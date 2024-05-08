11b0a6a3ee7ddf663a58f76b9f55038f22a56587
Cody <c.jmiller17@yahoo.com>
2024-05-08 14:39:29 -0400

    Most Functions added

--- card-game.py
+++ card-game.py
@@ -1,3 +1,4 @@
+import random
 '''
 Deck Class;
     Trump = some suit
@@ -46,28 +47,85 @@ The totals need to be compared and a winner needs to be checked for
 import Random
 '''
 class Player:
-    def __init__(self, dealer, number, name, score):
+    def __init__(self, name, dealer = False, number = 1, score = 0):
         self.dealer = dealer
         self.number = number
         self.name = name
         self.score = score
-        
-class Deck:
-    def __init__(self):
+        self.hand = []
 
+    def draw_card(self, deck):
+        self.hand.append(deck.draw_from_deck())
+        return self
+
+    def show_hand(self):
+        for card in self.hand:
+           card.show_card()
+
+    def discard_card(self):
+        return self.hand.pop()
+        
+
+class Deck:
+    def __init__(self, trump = False):
+        self.trump = trump
+        self.cards = []
+        self.discard_pile = []
+        self.build()
+
+    def build(self):
+        for suit in ["\u2660", "\u2663", "\u2665", "\u2666"]:
+            for value in range(1, 14):
+                self.cards.append(Card(value, suit))
+
+    def shuffle(self):
+        # First -1 means last card, 0 means decrementing towards 0 index, last -1 means 
+        for i in range(len(self.cards)-1, 0, -1):
+            random_card = random.randint(0, i)
+            self.cards[i], self.cards[random_card] = self.cards[random_card], self.cards[i]
+
+    def draw_from_deck(self):
+        #Pop acts as the top of the deck because it removes from the last item
+        return self.cards.pop()
+    
+    def determine_trump(self):
+        suits = ["\u2660", "\u2663", "\u2665", "\u2666"]
+        self.trump = random.choice(suits)
+    
+    def show_top_card(self):
+        for card in self.cards:
+            card.show_card()
+        self.determine_trump()               
         
 class Card:
-    def __init__(self, color, value, face_card, suit):
+    def __init__(self, value, suit, color = "red", face_card = False):
         self.color = color
         self.value = value
         self.face_card = face_card
         self.suit = suit
     
-    def show(self):
-        print "{} of {}".format(self.value, self.suit)
+    def show_card(self):
+        print("{}{}".format(self.value, self.suit))
 
 
-print("\u2665")
-print("\u2660")
-print("\u2663")
-print("\u2666")
+
+# Spade "\u2660"
+# Clubs "\u2663"
+# Heart "\u2665"
+# Diamonds "\u2666"
+
+# python card-game.py
+
+# card = Card(6, "\u2666")
+# card.show()
+
+deck = Deck()
+deck.shuffle()
+# deck.show()
+
+player = Player("Jeanine")
+player.draw_card(deck).draw_card(deck).draw_card(deck).draw_card(deck).draw_card(deck)
+player.show_hand()
+
+# card = deck.draw()
+# card.show()
