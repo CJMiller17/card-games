b1090b68bb883febeeb20cc6a052bad27a2afc73
Cody <c.jmiller17@yahoo.com>
2024-05-08 11:24:12 -0400

    File Set and Classes Created

---
+++ card-game.py
@@ -0,0 +1,54 @@
+'''
+Deck Class;
+
+
+Player Class:
+    Dealer = Boolean
+    Number = number
+    Name = input value
+    Score = {
+        "win":
+        "loss":
+        "draw":
+    }
+
+    Draw
+    Show
+    turn = boolean
+
+Card Class:
+    color = red or black
+    value/number = value of card
+    face_card = boolean
+    suit = heart, spades, diamonds, clubs
+
+def dealCard (turn)
+
+def total(turn)
+        
+Calculate Hand
+Check for winner
+
+deck = []
+playerHand = []
+dealerHand = []
+
+import Random
+'''
+class player:
+    def __init__(self, dealer, number, name, score):
+        self.dealer = dealer
+        self.number = number
+        self.name = name
+        self.score = score
+        
+class deck:
+    def __init__(self):
+
+        
+class card:
+    def __init__(self, color, value, face_card, suit):
+        self.color = color
+        self.value = value
+        self.face_card = face_card
+        self.suit = suit
