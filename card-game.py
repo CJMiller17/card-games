5ca7c1da3b7860481cf28ea2d66b13e5512e6bbc
Cody <c.jmiller17@yahoo.com>
2024-05-08 15:31:16 -0400

    Game is working

--- card-game.py
+++ card-game.py
@@ -1,39 +1,12 @@
 import random
 '''
-Deck Class;
-    Trump = some suit
-    Shuffle = 
-    Deal = deck minus the cards dealt
-    Discard pile = There usually is a discard pile
+# Spade "\u2660"
+# Clubs "\u2663"
+# Heart "\u2665"
+# Diamonds "\u2666"
 
-
-Player Class:
-    Dealer = Boolean
-    Number = number
-    Name = input value
-    Score = {
-        "win":
-        "loss":
-        "draw":
-    }
-
-    Draw
-    Show
-    turn = boolean
-
-Card Class:
-    color = red or black
-    value/number = value of card
-    face_card = boolean
-    suit = heart, spades, diamonds, clubs
-
-        
 Calculate Hand
 Check for winner
-
-deck = []
-playerHand = []
-dealerHand = []
 
 Thinking through the highest card game:
 The player and the dealer must each be dealt a card. 
@@ -47,11 +20,16 @@ The totals need to be compared and a winner needs to be checked for
 import Random
 '''
 class Player:
-    def __init__(self, name, dealer = False, number = 1, score = 0):
+    def __init__(self, name, turn = False, dealer = False, number = 1, score = 0):
         self.dealer = dealer
         self.number = number
         self.name = name
-        self.score = score
+        self.turn = turn
+        self.score = {
+            "wins" : 0,
+            "losses" : 0,
+            "draws" : 0,
+        }
         self.hand = []
 
     def draw_card(self, deck):
@@ -98,34 +76,36 @@ class Deck:
         self.determine_trump()               
         
 class Card:
-    def __init__(self, value, suit, color = "red", face_card = False):
-        self.color = color
+    def __init__(self, value, suit, face_card = False):
         self.value = value
         self.face_card = face_card
         self.suit = suit
+
+        if self.suit in ["\u2665", "\u2666"]: # Hearts or Diamonds
+            self.color = "red"
+        elif self.suit in ["\u2660", "\u2663"]: # Spade or Clubs
+            self.color = "black"
+        else:
+            self.color = "green"
     
     def show_card(self):
         print("{}{}".format(self.value, self.suit))
 
 
-
-# Spade "\u2660"
-# Clubs "\u2663"
-# Heart "\u2665"
-# Diamonds "\u2666"
-
-# python card-game.py
-
-# card = Card(6, "\u2666")
-# card.show()
-
 deck = Deck()
-deck.shuffle()
+deck.build()
+ 
+
+for item in deck.cards:
+    print(f'card: {item.value} {item.suit}{item.color}')
+    print('\n')
+
+# deck.shuffle()
 # deck.show()
 
-player = Player("Jeanine")
-player.draw_card(deck).draw_card(deck).draw_card(deck).draw_card(deck).draw_card(deck)
-player.show_hand()
+# player = Player("Jeanine")
+# player.draw_card(deck).draw_card(deck).draw_card(deck).draw_card(deck).draw_card(deck)
+# player.show_hand()
 
 # card = deck.draw()
 # card.show()
